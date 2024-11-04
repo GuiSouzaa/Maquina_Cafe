2ee853a5975eefc5126efdf148c869c62424c524
unknown <guilherme_souzamcd@hotmail.com>
2024-11-04 10:48:26 -0300

    refatorando

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from Maquina import Maquina
 from Cafe import Cafe
 from CafeLeite import CafeLeite
-from principal import ligar_maquina
 from principal import escolherTipo
 
 if __name__ == "__main__":
-    # maquina01 = Maquina(False)
-    # maquina01.ligar()
-
-    # #classe cafe
-    # maquinaCafe = Cafe('cafe')
-    # maquinaCafe.escolhido()
-
-    # #cafe com leite
-    # maquinaCafeComLeite = CafeLeite()
-    # maquinaCafeComLeite.escolhido()
-
-    maquina01 = Maquina(False)
+   
+    maquina01 = Maquina(False) #Estado da maquina ligado/desligado
     maquina01.ligar()
 
     maquina01.definir_acucar()
